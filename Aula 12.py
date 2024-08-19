9ffa6bc97311d81d480e47e0bb38bcb7a18444e0
DiegoPSP <diego.paulo2022@gmail.com>
2024-08-18 21:29:14 -0300

    Primeiro exercício do Mundo 2 de Python durante as explicações do Guanabara.

---
+++ Aula 12.py
@@ -0,0 +1,9 @@
+nome = str(input('Qual é seu nome? '))
+
+if nome == 'Diego':
+    print('Que nome lindo!')
+elif nome == 'Kênia':
+    print('Seu nome é diferente, mas também é bonito!')#Estrutura condicional aninhada, pode colocar quantos ELFI forem necessários;
+else:
+    print('Seu nome é bem normal.')
+print(f'Tenha um bom dia, {nome}!')
