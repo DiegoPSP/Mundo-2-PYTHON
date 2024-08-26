bc4de2c1dbe92db5a2853518b28968f0d9898512
DiegoPSP <diego.paulo2022@gmail.com>
2024-08-26 18:25:36 -0300

    Apresentar se o primeiro número é maior que o segundo

---
+++ Desafio 38.py
@@ -0,0 +1,9 @@
+numero = int(input("Digite um número: "))
+valor = int(input("Digite outro número: "))
+
+if numero > valor:
+    print(f"O primeiro número {numero} é MAIOR que {valor}")
+elif numero < valor:
+    print(f"O primeiro número {numero} é MENOR que {valor}!")
+else:
+    print(f"Os némeros são IGUAIS!")
